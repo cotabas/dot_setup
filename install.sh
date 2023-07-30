7ac8f8f4876379f64d6441e5e83caa9713fd6739
cotabas <cotabas@gmail.com>
2023-07-30 12:53:02 -0700

    added fonts

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ cp lk /usr/sbin/
 sudo chmod +x /usr/sbin/lk
 
 sudo pacman -Syu
-sudo pacman -S npm gcc unzip neovim starship tmux ripgrep exa 
+sudo pacman -S npm gcc unzip neovim starship tmux ripgrep exa bat git 
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
@@ -25,5 +25,7 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 git clone https://github.com/cotabas/nvim ~/.config/nvim/
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+sudo pacman -S noto-fonts bash-completion ttf-inconsolata-nerd ttf-inconsolata
 
 echo "prefix + I in tmux to load plugins"
