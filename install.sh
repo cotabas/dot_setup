64608aae29b386a6da41c33596b44ddedda3e094
cotabas <cotabas@gmail.com>
2023-08-05 22:06:26 -0700

    first run changes

--- install.sh
+++ install.sh
@@ -3,17 +3,22 @@ ln .bashrc ~/.bashrc
 ln .gitconfig ~/.gitconfig
 
 mkdir ~/.config/hypr
-ln hypr/* ~/.config/hypr/
+cp hypr/my_gruv_bg.png ~/.config/hypr/ 
+ln hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+ln hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
 
 mkdir ~/.config/waybar
-ln waybar/* ~/.config/waybar/
+ln waybar/config ~/.config/waybar/config
+ln waybar/pwr.sh ~/.config/waybar/pwr.sh
+ln waybar/style.css ~/.config/waybar/style.css
 
 mkdir ~/.config/kitty/
-ln kitty/* ~/.config/kitty/
+ln kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln kitty/gruvbox.conf ~/.config/kitty/gruvbox.conf
 
-ln starship.toml ~/.config/
+ln starship.toml ~/.config/starship.toml
 
-ln tmux.conf ~/.tmux/
+ln tmux.conf ~/.tmux/tmux.conf
 
 sudo cp lk /usr/sbin/
 sudo chmod +x /usr/sbin/lk
@@ -23,7 +28,7 @@ sudo systemctl enable wpa_init
 sudo systemctl start wpa_init
 
 sudo pacman -Syu
-sudo pacman -S npm gcc unzip neovim starship tmux ripgrep exa bat git 
+sudo pacman -S npm gcc unzip neovim starship tmux ripgrep exa bat
 
 sudo cp /usr/sbin/nvim /usr/sbin/vi
 
@@ -35,6 +40,8 @@ git clone https://github.com/cotabas/nvim ~/.config/nvim/
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 sudo pacman -S noto-fonts bash-completion ttf-inconsolata-nerd ttf-inconsolata powertop
+
+sudo pacman -S alsa-utils pulseaudio pulseaudio-bluetooth pulseaudio-alsa
 
 sudo cp powertop.service /etc/systemd/system/
 sudo systemctl enable powertop
