076db7dfc602173f8911d7a6155bc2e96b0f5a26
cotabas <cotabas@gmail.com>
2024-01-09 21:34:08 -0800

    add switching the nvim repo remote to ssh

--- install.sh
+++ install.sh
@@ -41,6 +41,10 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 git clone https://github.com/cotabas/nvim ~/.config/nvim/
+cd ~/.config/nvim/
+git remote set-url origin git@github.com:cotabas/nvim.git
+cd -
+echo "This might not work, throwing it in untested.."
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
